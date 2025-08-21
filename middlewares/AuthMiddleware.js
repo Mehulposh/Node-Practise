e9f14d3769f6dfa47b95530078203758dab5c9db
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-08-21 11:51:12 +0530

    Created Middleware

---
+++ middlewares/AuthMiddleware.js
@@ -0,0 +1,16 @@
+const password = process.env.PASSWORD;
+
+function AuthMiddleware(req,res,next){
+    const headers = req.headers;
+    const token = headers.authorization;
+    // console.log(token);
+
+    if(token === password){
+        next();
+    }else{
+        res.status(403).json({message : 'please send authorization '});
+    }
+} 
+
+
+module.exports = AuthMiddleware;
--- routes/HomeRoutes.js
+++ routes/HomeRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {HomeResponse,ContactResponse} = require('../controllers/HomeControllers');
+const AuthMiddleware = require('../middlewares/AuthMiddleware')
 const router = express.Router();
 
 router.get('/', HomeResponse);
 router.get('/home',HomeResponse);
-router.get('/contacts',ContactResponse);
+router.get('/contacts',AuthMiddleware,ContactResponse);
 
 module.exports = router;
